f4f11da6ec080efb0f3187fe374ef6999b91cf07
linos <smida.bassem@gmail.com>
2017-04-27 14:53:35 +0200

    format code

--- spark-word-count/src/main/scala/WordCount.scala
+++ spark-word-count/src/main/scala/WordCount.scala
@@ -1,23 +1,28 @@
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+
 /**
   * Created by linos on 26/04/17.
   */
 object WordCount {
 
-  //    StreamingExamples.setStreamingLogLevels()
-  def main(args:Array[String]) {
+
+  def main(args: Array[String]) {
     println("Hello world")
-    val sparkConf = new SparkConf().setAppName("WordCount").setMaster("localhost")
+    val sparkConf = new SparkConf().setAppName("WordCount").setMaster("local")
+
     // Create the context
     val ssc = new SparkContext(sparkConf);
-
-    // Create the FileInputDStream on the directory and use the
-    // stream to count words in new files created
+    //Create an RDD from texte file
     val lines = ssc.textFile("/home/linos/Documents/example_text.txt");
+    // Create a list of words using flatMap
+//    val words = lines.flatMap(word=>word.split(" "))
     val words = lines.flatMap(_.split(" "))
+    //1-Map each word with 1 and aggregate using reduceByKey
     val wordCounts = words.map(x => (x, 1)).reduceByKey(_ + _)
-   println( "the word count of "  +  wordCounts)
+    val wordCountWithAggregate = words.map(x=> (x,1)).aggregateByKey(0)((k,v) => v.toInt+k, (v,k) => k+v).collect
+    println("the word count of " + wordCounts)
+    println("the word count of aggregate " + wordCountWithAggregate.toList)
   }
 
 }
