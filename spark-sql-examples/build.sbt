a47e88ef2af3fd156d8db5a02bf15c8b0771c8bf
linos1003 <smida.bassem@gmail.com>
2017-04-29 22:19:41 +0200

    SQL select from jsonfile

--- spark-sql-examples/build.sbt
+++ spark-sql-examples/build.sbt
@@ -2,9 +2,9 @@ name := "spark-sql-examples"
 
 version := "1.0"
 
-scalaVersion := "2.12.2"
+scalaVersion := "2.10.4"
 
 libraryDependencies += "org.apache.spark" % "spark-streaming_2.10" % "2.1.0"
-libraryDependencies += "org.apache.spark" % "spark-core_2.10" % "1.5.2"
+libraryDependencies += "org.apache.spark" % "spark-core_2.10" % "2.1.0"
 
 libraryDependencies += "org.apache.spark" % "spark-sql_2.10" % "2.1.0"
--- spark-sql-examples/src/main/scala/BasicQuery.scala
+++ spark-sql-examples/src/main/scala/BasicQuery.scala
@@ -1,5 +1,4 @@
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.{SparkConf, SparkContext}
 
 /**
   * Created by linos on 29/04/17.
@@ -10,16 +9,11 @@ object BasicQuery {
 
   def main(args: Array[String]): Unit = {
     val sparkSqlContext = SparkSession.builder().appName("SparkQuery basic query")
-                                                .master("local")
-                                                .getOrCreate()
-
-
-
-    val input = sparkSqlContext.read.json("src/main/resources/cars1.json")
-    input.createOrReplaceTempView("Cars1")
-
-    val result = sparkSqlContext.sql("SELECT * FROM Cars1")
-
+                          .master("local").getOrCreate()
+    val input = sparkSqlContext.read.json("src/main/resources/cars.json")
+    input.createOrReplaceTempView("Cars")
+    val result = sparkSqlContext.sql("SELECT * FROM Cars")
     result.show()
   }
 }
+case class Cars(name: String)
